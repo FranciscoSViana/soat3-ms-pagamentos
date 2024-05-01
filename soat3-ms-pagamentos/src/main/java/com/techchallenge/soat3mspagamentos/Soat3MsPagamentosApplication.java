7bba1db38f33f8d473bcf53abfdbf8a03027fe68
VinniJr <vinni.jr@gmail.com>
2024-05-01 12:06:29 -0300

    Subiondo

---
+++ soat3-ms-pagamentos/src/main/java/com/techchallenge/soat3mspagamentos/Soat3MsPagamentosApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.soat3mspagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soat3MsPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soat3MsPagamentosApplication.class, args);
+	}
+
+}
---
+++ soat3-ms-pagamentos/src/test/java/com/techchallenge/soat3mspagamentos/Soat3MsPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.soat3mspagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soat3MsPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
